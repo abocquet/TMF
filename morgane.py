18b072ee8dec3f136f09cb785cb7eb75cadb97e7
Adrien Bocquet <adrienbocquet38@gmail.com>
2019-01-23 12:04:51 +0100

    fix limit condition for melting

--- morgane.py
+++ morgane.py
@@ -14,7 +14,7 @@ temps_total = 3600  # s
 # Béton
 hauteur_beton_sacrificiel = 0.5  # metres
 surface_beton = 99
-temperature_intiale_beton = 70 + 273 # K
+temperature_intiale_beton = 70 + 273  # K
 conductivite_beton = 1.9082
 temperature_fusion_beton = 1_500  # K
 chaleur_latente_beton = 640_000  # J/kg
@@ -29,14 +29,13 @@ conductivite_air = 0.08
 
 # Corium
 coefficient_echange_air_corium = 5  # W/(m2.K)
-masse_volumique_corium = 4600 # kg/m^3
-volume_corium = 59 # m^3
+masse_volumique_corium = 4600  # kg/m^3
+volume_corium = 59  # m^3
 masse_corium = masse_volumique_corium * volume_corium
 capacite_thermique_corium = 1500
 conductivite_corium = 3
 
-
-#Acier
+# Acier
 temperature_initiale_acier = 343
 masse_volumique_acier = 8000
 conductivite_thermique_acier = 50
@@ -47,23 +46,26 @@ epaisseur_acier = 0.04
 temperature_initiale_corium = 2273
 production_chaleur_corium = 35e6
 
-masse_volumique_air = 1.2 # kg/m^3
+masse_volumique_air = 1.2  # kg/m^3
 
-#-------------  Simulation
+# -------------  Simulation
 
 
 model = Model([
-    Element(temperature_initiale_air, masse_volumique_air, 6, capacite_thermique_air , surface_beton, conductivite_air,0),
+    Element(temperature_initiale_air, masse_volumique_air, 6, capacite_thermique_air, surface_beton, conductivite_air,
+            0),
     Exchange(h=5, radiations=False),
-    Element(temperature_initiale_corium, masse_volumique_corium, volume_corium/ surface_beton,capacite_thermique_corium, surface_beton, conductivite_corium, production_chaleur_corium),
+    Element(temperature_initiale_corium, masse_volumique_corium, volume_corium / surface_beton,
+            capacite_thermique_corium, surface_beton, conductivite_corium, production_chaleur_corium),
     SolidExchange(radiations=False),
-    MeltSlicedElement(temperature_intiale_beton, masse_volumique_beton,hauteur_beton_sacrificiel, surface_beton,cp_beton,conductivite_beton,10,temperature_fusion_beton,chaleur_latente_beton,0,False),
+    MeltSlicedElement(temperature_intiale_beton, masse_volumique_beton, hauteur_beton_sacrificiel, surface_beton,
+                      cp_beton, conductivite_beton, 2, temperature_fusion_beton, chaleur_latente_beton, 0, False),
     SolidExchange(radiations=False),
-    Element(temperature_initiale_acier,masse_volumique_acier,epaisseur_acier,capacite_thermique_corium, surface_acier,conductivite_thermique_acier,0)
-
+    Element(temperature_initiale_acier, masse_volumique_acier, epaisseur_acier, capacite_thermique_corium,
+            surface_acier, conductivite_thermique_acier, 0)
 
 ])
 
-time = model.run(timestep=1e0, time=3600)
+time = model.run(timestep=1e0, time=3600 * 10)
 plt.plot(time, model.layers[2].history["T"][0])
 plt.show()
