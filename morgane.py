16e3f4016b0d4cf9e3bc3f8ae0822cae954e6fce
MorganeGo <40238701+MorganeGo@users.noreply.github.com>
2019-01-24 11:16:56 +0100

    Update morgane.py

--- morgane.py
+++ morgane.py
@@ -61,17 +61,17 @@ model = Model([
      SolidExchange(radiations=False),
      #Element(temperature_intiale_beton, masse_volumique_corium, hauteur_beton_sacrificiel, capacite_thermique_beton, surface_beton, conductivite_beton,0),
      MeltSlicedElement(temperature_intiale_beton, masse_beton/ (hauteur_beton_sacrificiel*surface_beton), hauteur_beton_sacrificiel, surface_beton,
-                       capacite_thermique_beton, conductivite_beton, 10, temperature_fusion_beton, chaleur_latente_beton, 0),
+                       capacite_thermique_beton, conductivite_beton, 100, temperature_fusion_beton, chaleur_latente_beton, 0),
      SolidExchange(radiations=False),
-     Element(temperature_initiale_acier, masse_volumique_acier, epaisseur_acier, capacite_thermique_corium,
+     Element(temperature_initiale_acier, masse_volumique_acier, epaisseur_acier, capacite_thermique_acier,
            surface_acier, conductivite_thermique_acier, 0)
 
 ])
 
-time = model.run(timestep=1e0, time=300000)
-plt.plot(time, model.layers[0].history["T"])
+time = model.run(timestep=1e0, time=100000)
+plt.plot(time, model.layers[1].history["T"])
 plt.plot(time, model.layers[2].history["T"][0])
-plt.plot(time, model.layers[3].history["T"])
+plt.plot(time, model.layers[-1].history["T"])
 plt.show()
 
 print(np.argmax(np.array(model.layers[-1].history["T"]) > (1500 + 273)))
